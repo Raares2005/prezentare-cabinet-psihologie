529310766cb122f279a1fa37fc8054f92b9b58d0
Ream94 <94ream@gmail.com>
2022-11-04 20:18:57 +0200

    nav burger
    implemented the backdrop & made the burger button

--- scriptForAll.js
+++ scriptForAll.js
@@ -1,13 +1,21 @@
-let btn = document.getElementById('containerBurger');
+let btn = document.getElementById('containerLogoBurger');
+let backdrop = document.getElementById('backdrop');
 let open = false;
-if(btn){
+if(btn && backdrop) {
     btn.addEventListener('click', function(){
-        if(!open){
-            document.getElementById('burgerLine').classList.add('open');
+        if(!open) {
+            document.getElementById('containerLogoBurger').classList.add('open');
+            backdrop.style.display = 'block';
             open = true;
         } else {
-            document.getElementById('burgerLine').classList.remove('open');
+            document.getElementById('containerLogoBurger').classList.remove('open');
+            backdrop.style.display = 'none';
             open = false;
         }
+    });
+    backdrop.addEventListener('click', function() {
+        document.getElementById('containerLogoBurger').classList.remove('open');
+        backdrop.style.display = 'none';
+        open = false;
     });
 }
