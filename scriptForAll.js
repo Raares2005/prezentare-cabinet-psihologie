6761e8653380067c95f31fd3bbeabea9a5e92fa9
Ream94 <94ream@gmail.com>
2022-12-02 00:02:28 +0200

    N

--- scriptForAll.js
+++ scriptForAll.js
@@ -2,10 +2,12 @@ let btn = document.getElementById('containerLogoBurger');
 let bLogo = document.getElementById('containerLogoBurger');
 let bContainer = document.getElementById('burgerListContainer');
 let backdrop = document.getElementById('backdrop');
+let btnServicii = document.getElementById('btnServicii');
+let dropdown = document.getElementById('dropdown');
 let open = false;
 if(btn && backdrop) {
     btn.addEventListener('click', function(){
-        if(!open) {
+        if(!bLogo.classList.contains('open')) {
             bLogo.classList.add('open');
             backdrop.style.display = 'block';
             bContainer.classList.add('bContainerOpen');
@@ -22,5 +24,14 @@ if(btn && backdrop) {
         backdrop.style.display = 'none';
         bContainer.classList.remove('bContainerOpen');
         open = false;
+    });
+}
+
+if(btnServicii) {
+    btnServicii.addEventListener('mouseover', function() {
+        if(!dropdown.classList.contains('visible')) dropdown.classList.add('visible');
+    });
+    dropdown.addEventListener('mouseleave', function(){
+        dropdown.classList.remove('visible');
     });
 }
