08bfcacd93ac23b0b444842d52d9c009d9efe25a
Raares2005 <rares.croitoru8@gmail.com>
2022-12-11 01:01:49 +0200

    .

--- homepage-page/script.js
+++ homepage-page/script.js
@@ -1,63 +1,157 @@
 // Select all slides
-const slides = document.querySelectorAll(".slide");
 
-// loop through slides and set each slides translateX property to index * 100% 
-slides.forEach((slide, indx) => {
-<<<<<<< Updated upstream
-  slide.style.transform = `translateX(${indx * 100}%)`;
-});
-=======
-    slide.style.transform = `translateX(${indx * 100}%)`;
-  });
+window.onload = function () {
+    const slides = document.querySelectorAll(".slide");
 
-  // current slide counter
->>>>>>> Stashed changes
-let curSlide = 0;
+    // loop through slides and set each slides translateX property to index * 100% 
+    slides.forEach((slide, indx) => {
+        slide.style.transform = `translateX(${indx * 100}%)`;
+    });
+    let curSlide = 1;
 
-// select next slide button
-const nextSlide = document.querySelector(".btn-next");
+    // select next slide button
+    const nextSlide = document.querySelector(".btn-next");
 
-// add event listener and next slide functionality
-nextSlide.addEventListener("click", function () {
-     curSlide++;
 
-  slides.forEach((slide, indx) => {
-    slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
-  });
-<<<<<<< Updated upstream
-});
-let maxSlide = slides.length - 1;
+    // add event listener and next slide functionality
+    if (nextSlide) {
+        nextSlide.addEventListener("click", function () {
+            curSlide++;
 
-// add event listener and navigation functionality
-nextSlide.addEventListener("click", function () {
-  // check if current slide is the last and reset current slide
-  if (curSlide === maxSlide) {
-    curSlide = 0;
-  } else {
-    curSlide++;
-  }
+            slides.forEach((slide, indx) => {
+                slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+            });
+        });
+    }
 
-//   move slide by -100%
-  slides.forEach((slide, indx) => {
-    slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
-  });
-});
-// select prev slide button
-const prevSlide = document.querySelector(".btn-prev");
+    let maxSlide = slides.length - 1;
 
-// add event listener and navigation functionality
-prevSlide.addEventListener("click", function () {
-  // check if current slide is the first and reset current slide to last
-  if (curSlide === 0) {
-    curSlide = maxSlide;
-  } else {
-    curSlide--;
-  }
 
-  //   move slide by 100%
-  slides.forEach((slide, indx) => {
-    slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
-  });
-=======
->>>>>>> Stashed changes
-});
+    // add event listener and navigation functionality
+    if (nextSlide) {
+        nextSlide.addEventListener("click", swapper, false)
+    }
+    nextSlide.addEventListener("click", function () {
+        // check if current slide is the last and reset current slide
+        if (curSlide === maxSlide) {
+            curSlide = 0;
+        } else {
+            curSlide--;
+        }
+
+        //   move slide by -100%
+        slides.forEach((slide, indx) => {
+            slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+        });
+    });
+
+
+
+
+
+
+    // select prev slide button
+    const prevSlide = document.querySelector(".btn-prev");
+
+    // add event listener and navigation functionality
+    prevSlide.addEventListener("click", function () {
+        // check if current slide is the first and reset current slide to last
+        if (curSlide === 0) {
+            curSlide = maxSlide;
+        } else {
+            curSlide--;
+        }
+
+        //   move slide by 100%
+        slides.forEach((slide, indx) => {
+            slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+        });
+    });
+}
+
+
+
+//  SECOND EXEMPLE
+
+// var currentIndex = 0;
+// displaySlides(currentIndex);
+
+
+// function setSlides(num) {
+//     displaySlides(currentIndex += num);
+// }
+
+// function displaySlides(num) {
+//     var x;
+//     var slides = document.getElementsByClassName("slide");
+//     if (num > slides.length) { currentIndex = 1 }
+//     if (num < 1 ) { currentIndex = slides.length }
+//     for (x = 0; x < slides.length; x++) {
+//         slides[x].style.display = "none";
+//     }
+//     slides[currentIndex - 1].style.display = "block";
+// }
+// nextBtn.addEventListener("click", function () {
+//     currentImage++;
+//     if (currentImage > images.length - 1) {
+//       currentImage = 0;
+//     }
+//     showImage(currentImage);
+//   });
+
+//   // the prev button function
+//   prevBtn.addEventListener("click", function () {
+//     currentImage--;
+//     if (currentImage < 0) {
+//       currentImage = images.length - 1;
+//     }
+//     showImage(currentImage);
+//   });
+
+
+// THIRD EXEMPLE
+
+// the variables of the image slider
+
+// window.onload=function(){
+// const image = document.querySelector(".slide-image");
+// const prevBtn = document.querySelector(".btn btn-prev");
+// const nextBtn = document.querySelector(".btn btn-next");
+
+// // the image array
+// const images = [
+//   "./img/01-living-room-blue-theme.jpg",
+//   "./img/02-living-room-couch.jpg",
+//   "./img/03-living-room-red-theme.jpg",
+//   "./img/04-modern-kitchen.jpg",
+// ];
+
+// // the index of the image on page load
+// let currentImage = 0;
+
+// // the image details that shows when the webpage loads
+// window.addEventListener("DOMContentLoaded", showImage);
+
+// // function to select and change the image details
+// function showImage() {
+//   image.src = images[currentImage];
+// }
+
+// // the next button function
+// nextBtn.addEventListener("click", function () {
+//     currentImage++;
+//     if (currentImage > images.length - 1) {
+//       currentImage = 0;
+//     }
+//     showImage(currentImage);
+//   });
+
+//   // the prev button function
+//   prevBtn.addEventListener("click", function () {
+//     currentImage--;
+//     if (currentImage < 0) {
+//       currentImage = images.length - 1;
+//     }
+//     showImage(currentImage);
+//   });
+// } 
