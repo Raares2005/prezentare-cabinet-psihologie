0768c8f9a8e5fc5139e6c2a89350b1674187f825
Ream94 <94ream@gmail.com>
2022-12-18 01:04:26 +0200

    Progress dropdown

--- scriptForAll.js
+++ scriptForAll.js
@@ -1,37 +1,40 @@
-let btn = document.getElementById('containerLogoBurger');
-let bLogo = document.getElementById('containerLogoBurger');
-let bContainer = document.getElementById('burgerListContainer');
-let backdrop = document.getElementById('backdrop');
-let btnServicii = document.getElementById('btnServicii');
-let dropdown = document.getElementById('dropdown');
+const btn = document.getElementById("containerLogoBurger");
+const bLogo = document.getElementById("containerLogoBurger");
+const bContainer = document.getElementById("burgerListContainer");
+const backdrop = document.getElementById("backdrop");
+const btnServicii = document.getElementById("btnServicii");
+const dropdown = document.getElementById("dropdown");
+const dropDownLinks = document.getElementsByClassName("dropDownLinks");
+
 let open = false;
-if(btn && backdrop) {
-    btn.addEventListener('click', function(){
-        if(!bLogo.classList.contains('open')) {
-            bLogo.classList.add('open');
-            backdrop.style.display = 'block';
-            bContainer.classList.add('bContainerOpen');
-            open = true;
-        } else {
-            bLogo.classList.remove('open');
-            backdrop.style.display = 'none';
-            bContainer.classList.remove('bContainerOpen');
-            open = false;
-        }
-    });
-    backdrop.addEventListener('click', function() {
-        bLogo.classList.remove('open');
-        backdrop.style.display = 'none';
-        bContainer.classList.remove('bContainerOpen');
-        open = false;
-    });
+if (btn && backdrop) {
+  btn.addEventListener("click", function () {
+    if (!bLogo.classList.contains("open")) {
+      bLogo.classList.add("open");
+      backdrop.style.display = "block";
+      bContainer.classList.add("bContainerOpen");
+      open = true;
+    } else {
+      bLogo.classList.remove("open");
+      backdrop.style.display = "none";
+      bContainer.classList.remove("bContainerOpen");
+      open = false;
+    }
+  });
+  backdrop.addEventListener("click", function () {
+    bLogo.classList.remove("open");
+    backdrop.style.display = "none";
+    bContainer.classList.remove("bContainerOpen");
+    open = false;
+  });
 }
 
 if(btnServicii) {
-    btnServicii.addEventListener('mouseover', function() {
-        if(!dropdown.classList.contains('visible')) dropdown.classList.add('visible');
-    });
-    dropdown.addEventListener('mouseleave', function(){
-        dropdown.classList.remove('visible');
-    });
+  dropdown.addEventListener("mouseover", function() {
+    for(let i = 0; i < dropDownLinks.length; i++) {
+      link = dropDownLinks[i].addEventListener("mouseleave", function(){
+        link.classList.remove("show");
+      });
+    }
+  });
 }
